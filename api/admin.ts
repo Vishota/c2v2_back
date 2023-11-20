26cbc2c04073c249bdbb1b114cff629eed536dbd
Vishota <88852731+Vishota@users.noreply.github.com>
2023-11-20 11:09:53 +0300

    setTeachersActive

--- api/admin.ts
+++ api/admin.ts
@@ -2,6 +2,7 @@ import { Handler } from "express";
 import { checkKeys } from "../misc/api";
 import { checkAdmin } from "../misc/auth";
 import { setUserAccess } from "../data/accounts";
+import { addTeacher, setTeacherActive } from "../data/teachers";
 
 export default {
     '/admin/ban': async (req, res, next) => {
@@ -25,5 +26,15 @@ export default {
         res.send({
             success: await setUserAccess(parseInt(request.id), true, admin.isAdmin.prime)
         })
+    },
+    '/admin/setTeacherActive': async (req, res, next) => {
+        const request = checkKeys(req.query as any, ['id'], ['active', 'inactive'])
+        const admin = await checkAdmin(req, res, next)
+        if(!admin.isAdmin) {
+            res.send({ success: false, info: 'not_admin' });
+            return false;
+        }
+        // 'active' passed in query => set active, else inactive
+        res.send({ success: await setTeacherActive(parseInt(request.id), 'active' in request) })
     }
 } as { [url: string]: Handler }
---
+++ data/teachers.ts
@@ -0,0 +1,17 @@
+import db from "../infrastructure/db"
+
+export async function addTeacher(userId: number, data: {
+    name: string,
+    speciality: string,
+    about: string
+}) {
+    const dbResponse = await db.query('INSERT INTO teachers("user_id", "name", "speciality", "about") VALUES ($1, $2, $3, $4)', [userId, data.name, data.speciality, data.about]);
+
+    return dbResponse.rowCount == 1
+}
+
+export async function setTeacherActive(userId: number, active: boolean) {
+    const dbResponse = await db.query('UPDATE teachers SET active=$2 WHERE user_id=$1', [userId, active]);
+
+    return dbResponse.rowCount == 1
+}
