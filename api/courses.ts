8e2e2b146eb3c8d1dd3b9b192a6c9f5a1a048c5d
Vishota <88852731+Vishota@users.noreply.github.com>
2023-11-21 23:59:20 +0300

    course show, hide, get

--- api/courses.ts
+++ api/courses.ts
@@ -1,7 +1,8 @@
 import { Handler } from "express";
 import { checkKeys } from "../misc/api";
-import { addCourse } from "../data/courses";
-import { checkTeacher } from "../misc/auth";
+import { addCourse, getCourse, setCourseAccessible } from "../data/courses";
+import { checkAdmin, checkTeacher } from "../misc/auth";
+import { setContentAccessible } from "../data/content";
 
 export default {
     '/courses/new': async (req, res, next) => {
@@ -9,5 +10,22 @@ export default {
         const teacher = await checkTeacher(req, res, next)
         if (!teacher.id) throw 'not_a_teacher'
         res.send({ id: await addCourse(teacher.id, request.title, request.about) })
+    },
+    '/courses/show': async (req, res, next) => {
+        const request = checkKeys(req.query as any, ['id'])
+        const admin = await checkAdmin(req, res, next)
+        if (!admin.id) throw 'no_auth'
+        res.send({ success: await setCourseAccessible(parseInt(request.id), true, admin.isAdmin ? 'ADMIN' : admin.id) });
+    },
+    '/courses/hide': async (req, res, next) => {
+        const request = checkKeys(req.query as any, ['id'])
+        const admin = await checkAdmin(req, res, next)
+        if (!admin.id) throw 'no_auth'
+        res.send({ success: await setCourseAccessible(parseInt(request.id), false, admin.isAdmin ? 'ADMIN' : admin.id) });
+    },
+    '/courses/get': async (req, res, next) => {
+        const request = checkKeys(req.query as any, ['id'])
+        const admin = await checkAdmin(req, res, next)
+        res.send({course: await getCourse(parseInt(request.id), admin.isAdmin ? 'ADMIN' : admin.id ? admin.id : null)})
     }
 } as { [url: string]: Handler }
--- data/courses.ts
+++ data/courses.ts
@@ -5,3 +5,21 @@ export async function addCourse(ownerUserId: number, title: string, about: strin
         [ownerUserId, title, about])
     return dbResponse.rowCount ? dbResponse.rows[0].id as number : false
 }
+export async function getCourse(courseId: number, as: number | 'ADMIN' | null) {
+    const dbResponse = as == 'ADMIN' ? 
+        await db.query('SELECT * FROM courses WHERE id=$1', [courseId])
+        :
+        await db.query('SELECT * FROM courses WHERE id=$1 AND (accessible=TRUE OR owner_user_id=$2)', [courseId, as]);
+    return dbResponse.rowCount ? dbResponse.rows[0] : false
+}
+export async function setCourseAccessible(contentId: number, accessible: boolean, as: number|'ADMIN'): Promise<boolean> {
+    // possible if either user has rights or he is the owner
+    if(as == 'ADMIN') {
+        const dbResponse = await db.query('UPDATE courses SET accessible=$2 WHERE id=$1',
+            [contentId, accessible])
+        return dbResponse.rowCount != 0
+    }
+    const dbResponse = await db.query('UPDATE courses SET accessible=$2 WHERE id=$1 AND owner_user_id=$3',
+        [contentId, accessible, as])
+    return dbResponse.rowCount != 0
+}
